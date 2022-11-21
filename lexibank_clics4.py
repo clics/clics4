6b814a81e3a59b603e8b422f2753167dbbdfc7e4
lingulist <mattis.list@lingpy.org>
2022-11-21 16:22:23 +0100

    update

--- lexibank_clics4.py
+++ lexibank_clics4.py
@@ -9,6 +9,8 @@ from cltoolkit import Wordlist
 from git import Repo
 from clldutils.misc import slug
 
+from pyconcepticon import Concepticon
+
 from pylexibank import Concept, Lexeme, Language, progressbar
 import attr
 
@@ -16,8 +18,7 @@ LANGUAGES = 250
 
 @attr.s
 class CustomConcept(Concept):
-    Tag = attr.ib(default=None)
-
+    Frequency = attr.ib(default=None)
 
 @attr.s
 class CustomLexeme(Lexeme):
@@ -67,12 +68,25 @@ class Dataset(BaseDataset):
         
     def cmd_makecldf(self, args):
 
+        # concepticongloss 2 id
+        gloss2id = {concept.gloss: concept.id for concept in
+                self.concepticon.conceptsets.values()}
+        args.log.info("created concepticon gloss to ID converter")
+
         datasets = [pycldf.Dataset.from_metadata(
             self.raw_dir / ds["ID"] / "cldf/cldf-metadata.json") for ds in
             self.etc_dir.read_csv("datasets.tsv", delimiter="\t",
-                dicts=True)]
+                dicts=True)][:5] # DEBUGGING
         args.log.info("loaded datasets")
         wl = Wordlist(datasets, ts=args.clts.api.bipa)
+
+
+        # read target concepts
+        targets = {}
+        for row in self.etc_dir.read_csv("concept-modifications.tsv",
+                delimiter="\t", dicts=True):
+            if row["Status"] in ["edited", "accepted"]:
+                targets[row["Source"]] = row["Targets"].split(" // ")
 
         # sort the concepts by number of unique glottocodes
         all_concepts = sorted(
@@ -91,48 +105,56 @@ class Dataset(BaseDataset):
                             concept.concepticon_gloss,
                             str(len(set([form.language.glottocode for form in
                                 concept.forms_with_sounds])))])+"\n")
-        args.log.info("wrote concepts to file")
-        input("bis hier")
-        unmerge = collections.defaultdict(set)
-        for concept in self.concepts:
-            for gloss in concept["BROADER_CONCEPT"].split(" // "):
-                unmerge[gloss].add(concept["CONCEPTICON_GLOSS"])
-
-        selected_concepts = [concept.id for concept in all_concepts if
-                concept.id not in unmerge][:1500]
+        args.log.info("wrote concept frequencies to file")
+        
+        selected_concepts = []
+        i = 0
+        for concept in all_concepts:
+            if concept.id in targets:
+                selected_concepts.extend(targets[concept.id])
+            else:
+                selected_concepts.append(concept.id)
+        selected_concepts = selected_concepts[:1500]
+        concept_count = {}
+        for concept in all_concepts:
+            if concept.id in selected_concepts:
+                concept_count[concept.id] = 1
+            elif concept.id in targets:
+                if [c for c in targets[concept.id] if c in selected_concepts]:
+                    concept_count[concept.id] = 0
+                    for t in targets[concept.id]:
+                        if t in selected_concepts:
+                            concept_count[concept.id] += 1
+        args.log.info("calculated frequencies for concepts ({0}/{1})".format(
+            len(concept_count), sum(concept_count.values())))
 
         # get valid languages
         valid_languages = []
         visited = set()
         for language in sorted(
                 wl.languages,
-                key=lambda x: len([c for c in x.concepts if c.id in
-                    selected_concepts]),
-                reverse=True):
+                key=lambda x: sum([concept_count[c.id] for c in x.concepts if c.id in
+                    concept_count]),
+                reverse=True
+                ):
             if language.glottocode in visited:
                 pass
             else:
-                cov = sum([
-                    len(unmerge.get(c.id, [""])) for c in language.concepts if c.id in unmerge or \
-                                c.id in selected_concepts]) 
+                cov = sum([concept_count[c.id] for c in language.concepts if
+                    c.id in concept_count])
                 if language.latitude and cov >= LANGUAGES:
                     visited.add(language.glottocode)
                     valid_languages.append(language.id)
-
         args.log.info("found {0} valid languages".format(len(valid_languages)))
 
-        concepts = {concept["CONCEPTICON_GLOSS"]: concept["TAG"] for concept in
-                self.concepts}
-                        
-        for concept in self.concepts:
-            if concept["CONCEPTICON_GLOSS"] in selected_concepts:
-                args.writer.add_concept(
-                        ID=slug(concept["CONCEPTICON_GLOSS"], lowercase=False),
-                        Name=concept["ENGLISH"],
-                        Concepticon_ID=concept["CONCEPTICON_ID"],
-                        Concepticon_Gloss=concept["CONCEPTICON_GLOSS"],
-                        Tag=concept["TAG"]
-                        )
+        # TODO: add all concepts already here TODO
+        for concept in wl.concepts:
+            args.writer.add_concept(
+                    ID=slug(concept.id, lowercase=False),
+                    Name=concept.name,
+                    Concepticon_ID=concept.concepticon_id,
+                    Concepticon_Gloss=concept.concepticon_gloss,
+                    )
         args.log.info("added concepts")
         
         new_concepts = {}
@@ -160,8 +182,8 @@ class Dataset(BaseDataset):
                         cnc_count += 1
                         frm_count += 1
                     elif (form.concept and form.concept.concepticon_gloss in
-                            unmerge):
-                        for gloss in unmerge[form.concept.concepticon_gloss]:
+                            targets):
+                        for gloss in targets[form.concept.concepticon_gloss]:
                             if gloss in selected_concepts:
                                 clics += [[
                                         language.id,
